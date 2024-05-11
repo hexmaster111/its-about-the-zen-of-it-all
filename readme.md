ba60cad0101cac7da7070d275cd501b0f5477624
hailey g <MY_NAME@example.com>
2024-05-11 18:55:13 -0500

    cleanup, and readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# breath
+
+![sometimes i just need to take a breath and calm down, and this can help.](breath.gif)
+
+sometimes i just need to take a breath and calm down, and this can help.
--- src/breath.c
+++ src/breath.c
@@ -4,7 +4,8 @@
 int main(int argc, char **argv)
 {
     const int disp_width = 1024,
-              disp_height = 700;
+              disp_height = 700,
+              font_size = 60;
 
     InitWindow(disp_width, disp_height, "Breath");
     SetTargetFPS(60);
@@ -56,9 +57,9 @@ int main(int argc, char **argv)
         BeginDrawing();
         ClearBackground(BLACK);
         DrawRectangleLines(x, y, rect_width, rect_heigh, LIGHTGRAY);
-        DrawCircle(circle_center_x, circle_center_y, circle_size, WHITE);
-        DrawText(words, x + rect_width / 2 - MeasureText(words, 18) / 2,
-                 y + rect_heigh / 2, 18, YELLOW);
+        DrawCircle(circle_center_x, circle_center_y, circle_size, LIGHTGRAY);
+        DrawText(words, x + rect_width / 2 - MeasureText(words, font_size) / 2,
+                 (y + rect_heigh / 2) - font_size / 2, font_size, YELLOW);
         EndDrawing();
 
         if (++frame > side_len)
